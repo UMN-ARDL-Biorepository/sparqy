a5c75e09b689d2d7b8784147774d55aad548a7dc
Brian Dunnette <dunn0172@umn.edu>
2025-10-01 09:32:13 -0500

    Add factory-boy and faker to development dependencies and update test structure

--- tests.py
+++ tests.py
@@ -1,8 +1,25 @@
+import pandas as pd
+import numpy as np
+
+from .main import extract_sampleid
+
+
+class Specimen:
+    def __init__(self, comments):
+        self.Comments = comments
+
+
+class SpecimenDF:
+    def __init__(self, specimens):
+        self.df = pd.DataFrame([specimen.__dict__ for specimen in specimens])
+
+
 def test_extract_sampleid():
-    # Add your test code here
-    pass
-
-
-def test_flag_viable():
-    # Add your test code here
-    pass
+    specimen_df = SpecimenDF(
+        [Specimen("SAMPLEID:12345, Other info"), Specimen("No sample id here")]
+    )
+    print(specimen_df.df)
+    extract_sampleid(specimen_df.df)
+    assert specimen_df.df["SAMPLEID"].iloc[0] is not None
+    assert specimen_df.df["SAMPLEID"].iloc[0] == "12345"
+    assert specimen_df.df["SAMPLEID"].iloc[1] is np.nan
